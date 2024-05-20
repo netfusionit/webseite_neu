9c3cb0ad88198eb74b81e442cbda5588c4c71891
Florian Scheibel <florian.scheibel@outlook.de>
2024-05-20 19:44:27 +0200

    Neue Testseite

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Show the modal on page load
-    var infoModal = new bootstrap.Modal(document.getElementById('infoModal'), {});
-    infoModal.show();
+// Add your custom JavaScript here
 
-    // Breadcrumb dynamic generation
-    var breadcrumb = document.getElementById('breadcrumb');
-    var pathArray = window.location.pathname.split('/');
-    var breadcrumbHTML = '<li class="breadcrumb-item"><a href="/">Home</a></li>';
-    var path = '';
-
-    for (var i = 1; i < pathArray.length; i++) {
-        path += '/' + pathArray[i];
-        if (i === pathArray.length - 1) {
-            breadcrumbHTML += `<li class="breadcrumb-item active" aria-current="page">${pathArray[i]}</li>`;
-        } else {
-            breadcrumbHTML += `<li class="breadcrumb-item"><a href="${path}">${pathArray[i]}</a></li>`;
-        }
-    }
-
-    breadcrumb.innerHTML = breadcrumbHTML;
-
-    // Dark mode toggle
-    var darkModeToggle = document.getElementById('darkModeToggle');
-    darkModeToggle.addEventListener('click', function () {
-        document.body.classList.toggle('dark-mode');
+// Example: Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
 });
