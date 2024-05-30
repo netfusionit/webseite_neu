8e5153ef31d8c2b02aaf88f07cec54c1f7494000
Florian Scheibel <florian.scheibel@outlook.de>
2024-05-30 13:47:52 +0200

    Pfadanpassungen

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NetFusion IT Services</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
