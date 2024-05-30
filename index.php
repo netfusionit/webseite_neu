e6973c4784654b51e475af7de612c0fa160cf38b
Florian Scheibel <florian.scheibel@outlook.de>
2024-05-30 21:57:06 +0200

    Neue Elemente

--- index.php
+++ index.php
@@ -50,10 +50,69 @@
             <span class="sr-only">Next</span>
         </a>
     </div>
+    
     <div class="container mt-5">
         <h1>Willkommen bei NetFusionIT</h1>
         <p>Wir bieten Internetdienstleistungen, Netzwerktechnik, Webdesign (Wordpress) und Service und Wartung.</p>
     </div>
+    
+    <div class="container mt-5">
+        <section id="about-us">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="icon-box">
+                        <img src="/img/icon1.png" alt="Icon 1">
+                        <h3>Lorem Ipsum</h3>
+                        <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="icon-box">
+                        <img src="/img/icon2.png" alt="Icon 2">
+                        <h3>Dolor Sit Amet</h3>
+                        <p>Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="icon-box">
+                        <img src="/img/icon3.png" alt="Icon 3">
+                        <h3>Sed ut perspiciatis</h3>
+                        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row mt-5">
+                <div class="col-md-6">
+                    <h2>Unleashing Potential with Creative Strategy</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <ul class="list-unstyled">
+                        <li><i class="fa fa-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+                        <li><i class="fa fa-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+                        <li><i class="fa fa-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+                    </ul>
+                    <a href="#more" class="btn btn-primary">Read More</a>
+                </div>
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <img src="/img/image1.jpg" class="img-fluid" alt="Image 1">
+                        </div>
+                        <div class="col-md-6">
+                            <img src="/img/image2.jpg" class="img-fluid" alt="Image 2">
+                        </div>
+                        <div class="col-md-6 mt-4">
+                            <img src="/img/image3.jpg" class="img-fluid" alt="Image 3">
+                        </div>
+                        <div class="col-md-6 mt-4">
+                            <img src="/img/image4.jpg" class="img-fluid" alt="Image 4">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    
     <?php include 'footer.php'; ?>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
