9596277bfbc3a0f9ba63808affea8676c26022b6
Florian Scheibel <florian.scheibel@outlook.de>
2024-05-31 20:04:05 +0200

    0

--- fetch_popup.php
+++ fetch_popup.php
@@ -1,7 +1,6 @@
 <?php
 include 'db.php';
 
-$show_popup = true;
 $popup_message = '';
 
 // Überprüfen, ob ein Popup angezeigt werden soll
@@ -10,17 +9,9 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     $popup = $result->fetch_assoc();
     $popup_message = $popup['content'];
-    $show_popup = true;
 }
 
 $conn->close();
 
-#session_start();
-#if ($show_popup) {
-#    if (!isset($_SESSION['last_popup_time']) || (time() - $_SESSION['last_popup_time']) >= 14400) {
-#        $_SESSION['last_popup_time'] = time();
-#        echo $popup_message;
-#    }
-#}
-
+echo $popup_message;
 ?>
