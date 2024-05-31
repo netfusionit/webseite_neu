65f9d40c90efe37bd7ec9f32e988340aa1bd22ac
Florian Scheibel <florian.scheibel@outlook.de>
2024-05-31 13:42:09 +0200

    Update

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 <body>
     <?php include 'header.php'; ?>
     <div class="alert alert-primary" role="alert">
-  Willkommen bei NetFusionIT! <br>
-  Unsere Webseite befindet sich derzeit im Aufbau!
-</div>
+        Willkommen bei NetFusionIT! <br>
+        Unsere Webseite befindet sich derzeit im Aufbau!
+    </div>
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
             <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -91,178 +91,138 @@
     </section><!-- /Featured Services Section -->
 
    <!-- About Section -->
-<section id="about" class="about section mt-5">
-    <div class="container">
-        <div class="row gy-4">
-            <div class="col-lg-6 content" data-aos="fade-up" data-aos-delay="100">
-                <p class="who-we-are">Who We Are</p>
-                <h3>Unleashing Potential with Creative Strategy</h3>
-                <p class="fst-italic">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                    magna aliqua.
-                </p>
-                <ul>
-                    <li><i class="bi bi-check-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></li>
-                    <li><i class="bi bi-check-circle"></i> <span>Duis aute irure dolor in reprehenderit in voluptate velit.</span></li>
-                    <li><i class="bi bi-check-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</span></li>
-                </ul>
-                <a href="#" class="read-more"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
-            </div>
-            <div class="col-lg-6 about-images" data-aos="fade-up" data-aos-delay="200">
-                <div class="row gy-4">
-                    <div class="col-lg-6">
-                        <img src="/img/placeholder2.jpg" class="img-fluid" alt="">
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="row gy-4">
-                            <div class="col-lg-12 mb-2">
-                                <img src="/img/placeholder.jpg" class="img-fluid" alt="">
-                            </div>
-                            <div class="col-lg-12">
-                                <img src="/img/placeholder.jpg" class="img-fluid" alt="">
+    <section id="about" class="about section mt-5">
+        <div class="container">
+            <div class="row gy-4">
+                <div class="col-lg-6 content" data-aos="fade-up" data-aos-delay="100">
+                    <p class="who-we-are">Who We Are</p>
+                    <h3>Unleashing Potential with Creative Strategy</h3>
+                    <p class="fst-italic">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua.
+                    </p>
+                    <ul>
+                        <li><i class="bi bi-check-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></li>
+                        <li><i class="bi bi-check-circle"></i> <span>Duis aute irure dolor in reprehenderit in voluptate velit.</span></li>
+                        <li><i class="bi bi-check-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</span></li>
+                    </ul>
+                    <a href="#" class="read-more"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
+                </div>
+                <div class="col-lg-6 about-images" data-aos="fade-up" data-aos-delay="200">
+                    <div class="row gy-4">
+                        <div class="col-lg-6">
+                            <img src="/img/placeholder2.jpg" class="img-fluid" alt="">
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="row gy-4">
+                                <div class="col-lg-12 mb-2">
+                                    <img src="/img/placeholder.jpg" class="img-fluid" alt="">
+                                </div>
+                                <div class="col-lg-12">
+                                    <img src="/img/placeholder.jpg" class="img-fluid" alt="">
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section><!-- /About Section -->
+    </section><!-- /About Section -->
 
     <!-- Features Section -->
     <section id="features" class="features section">
+        <div class="container section-title" data-aos="fade-up">
+            <h2>Features</h2>
+            <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+        </div><!-- End Section Title -->
 
-      <!-- Section Title -->
-      <div class="container section-title" data-aos="fade-up">
-        <h2 class="">Features</h2>
-        <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
-      </div><!-- End Section Title -->
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-lg-5 d-flex align-items-center">
+                    <ul class="nav nav-tabs" data-aos="fade-up" data-aos-delay="100">
+                        <li class="nav-item">
+                            <a class="nav-link active show" data-bs-toggle="tab" data-bs-target="#features-tab-1">
+                                <i class="bi bi-binoculars"></i>
+                                <div>
+                                    <h4 class="d-none d-lg-block">Modi sit est dela pireda nest</h4>
+                                    <p>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-bs-toggle="tab" data-bs-target="#features-tab-2">
+                                <i class="bi bi-box-seam"></i>
+                                <div>
+                                    <h4 class="d-none d-lg-block">Unde praesenti mara setra le</h4>
+                                    <p>Recusandae atque nihil. Delectus vitae non similique magnam molestiae sapiente similique tenetur aut voluptates sed voluptas ipsum voluptas</p>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-bs-toggle="tab" data-bs-target="#features-tab-3">
+                                <i class="bi bi-brightness-high"></i>
+                                <div>
+                                    <h4 class="d-none d-lg-block">Pariatur explica nitro dela</h4>
+                                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum Debitis nulla est maxime voluptas dolor aut</p>
+                                </div>
+                            </a>
+                        </li>
+                    </ul><!-- End Tab Nav -->
+                </div>
 
-      <div class="container">
-        <div class="row justify-content-between">
-
-          <div class="col-lg-5 d-flex align-items-center">
-
-            <ul class="nav nav-tabs" data-aos="fade-up" data-aos-delay="100">
-              <li class="nav-item">
-                <a class="nav-link active show" data-bs-toggle="tab" data-bs-target="#features-tab-1">
-                  <i class="bi bi-binoculars"></i>
-                  <div>
-                    <h4 class="d-none d-lg-block">Modi sit est dela pireda nest</h4>
-                    <p>
-                      Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                      velit esse cillum dolore eu fugiat nulla pariatur
-                    </p>
-                  </div>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="tab" data-bs-target="#features-tab-2">
-                  <i class="bi bi-box-seam"></i>
-                  <div>
-                    <h4 class="d-none d-lg-block">Unde praesenti mara setra le</h4>
-                    <p>
-                      Recusandae atque nihil. Delectus vitae non similique magnam molestiae sapiente similique
-                      tenetur aut voluptates sed voluptas ipsum voluptas
-                    </p>
-                  </div>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="tab" data-bs-target="#features-tab-3">
-                  <i class="bi bi-brightness-high"></i>
-                  <div>
-                    <h4 class="d-none d-lg-block">Pariatur explica nitro dela</h4>
-                    <p>
-                      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                      Debitis nulla est maxime voluptas dolor aut
-                    </p>
-                  </div>
-                </a>
-              </li>
-            </ul><!-- End Tab Nav -->
-
-          </div>
-
-          <div class="col-lg-6">
-
-            <div class="tab-content" data-aos="fade-up" data-aos-delay="200">
-
-              <div class="tab-pane fade active show" id="features-tab-1">
-                <img src="assets/img/tabs-1.jpg" alt="" class="img-fluid">
-              </div><!-- End Tab Content Item -->
-
-              <div class="tab-pane fade" id="features-tab-2">
-                <img src="assets/img/tabs-2.jpg" alt="" class="img-fluid">
-              </div><!-- End Tab Content Item -->
-
-              <div class="tab-pane fade" id="features-tab-3">
-                <img src="assets/img/tabs-3.jpg" alt="" class="img-fluid">
-              </div><!-- End Tab Content Item -->
+                <div class="col-lg-6">
+                    <div class="tab-content" data-aos="fade-up" data-aos-delay="200">
+                        <div class="tab-pane fade active show" id="features-tab-1">
+                            <img src="assets/img/tabs-1.jpg" alt="" class="img-fluid">
+                        </div><!-- End Tab Content Item -->
+                        <div class="tab-pane fade" id="features-tab-2">
+                            <img src="assets/img/tabs-2.jpg" alt="" class="img-fluid">
+                        </div><!-- End Tab Content Item -->
+                        <div class="tab-pane fade" id="features-tab-3">
+                            <img src="assets/img/tabs-3.jpg" alt="" class="img-fluid">
+                        </div><!-- End Tab Content Item -->
+                    </div>
+                </div>
             </div>
-
-          </div>
-
         </div>
-
-      </div>
-
     </section><!-- /Features Section -->
 
     <!-- Features Details Section -->
     <section id="features-details" class="features-details section">
+        <div class="container">
+            <div class="row gy-4 justify-content-between features-item">
+                <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+                    <img src="assets/img/features-1.jpg" class="img-fluid" alt="">
+                </div>
+                <div class="col-lg-5 d-flex align-items-center" data-aos="fade-up" data-aos-delay="200">
+                    <div class="content">
+                        <h3>Corporis temporibus maiores provident</h3>
+                        <p>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
+                        <a href="#" class="btn more-btn">Learn More</a>
+                    </div>
+                </div>
+            </div><!-- Features Item -->
 
-      <div class="container">
-
-        <div class="row gy-4 justify-content-between features-item">
-
-          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-            <img src="assets/img/features-1.jpg" class="img-fluid" alt="">
-          </div>
-
-          <div class="col-lg-5 d-flex align-items-center" data-aos="fade-up" data-aos-delay="200">
-            <div class="content">
-              <h3>Corporis temporibus maiores provident</h3>
-              <p>
-                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
-              </p>
-              <a href="#" class="btn more-btn">Learn More</a>
-            </div>
-          </div>
-
-        </div><!-- Features Item -->
-
-        <div class="row gy-4 justify-content-between features-item">
-
-          <div class="col-lg-5 d-flex align-items-center order-2 order-lg-1" data-aos="fade-up" data-aos-delay="100">
-
-            <div class="content">
-              <h3>Neque ipsum omnis sapiente quod quia dicta</h3>
-              <p>
-                Quidem qui dolore incidunt aut. In assumenda harum id iusto lorena plasico mares
-              </p>
-              <ul>
-                <li><i class="bi bi-easel flex-shrink-0"></i> Et corporis ea eveniet ducimus.</li>
-                <li><i class="bi bi-patch-check flex-shrink-0"></i> Exercitationem dolorem sapiente.</li>
-                <li><i class="bi bi-brightness-high flex-shrink-0"></i> Veniam quia modi magnam.</li>
-              </ul>
-              <p></p>
-              <a href="#" class="btn more-btn">Learn More</a>
-            </div>
-
-          </div>
-
-          <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-up" data-aos-delay="200">
-            <img src="assets/img/features-2.jpg" class="img-fluid" alt="">
-          </div>
-
-        </div><!-- Features Item -->
-
-      </div>
-
+            <div class="row gy-4 justify-content-between features-item">
+                <div class="col-lg-5 d-flex align-items-center order-2 order-lg-1" data-aos="fade-up" data-aos-delay="100">
+                    <div class="content">
+                        <h3>Neque ipsum omnis sapiente quod quia dicta</h3>
+                        <p>Quidem qui dolore incidunt aut. In assumenda harum id iusto lorena plasico mares</p>
+                        <ul>
+                            <li><i class="bi bi-easel flex-shrink-0"></i> Et corporis ea eveniet ducimus.</li>
+                            <li><i class="bi bi-patch-check flex-shrink-0"></i> Exercitationem dolorem sapiente.</li>
+                            <li><i class="bi bi-brightness-high flex-shrink-0"></i> Veniam quia modi magnam.</li>
+                        </ul>
+                        <p></p>
+                        <a href="#" class="btn more-btn">Learn More</a>
+                    </div>
+                </div>
+                <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-up" data-aos-delay="200">
+                    <img src="assets/img/features-2.jpg" class="img-fluid" alt="">
+                </div>
+            </div><!-- Features Item -->
+        </div>
     </section><!-- /Features Details Section -->
-
 
     <?php include 'footer.php'; ?>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
