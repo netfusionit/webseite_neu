0f72e2fbab390473a3a5793ab84cece02bbe8657
Florian Scheibel <florian.scheibel@outlook.de>
2024-05-31 21:10:49 +0200

    Update

--- header.php
+++ header.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NetFusionIT</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="styles.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+</head>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.4.1/font/bootstrap-icons.min.css">
 <script src="script.js"></script>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
