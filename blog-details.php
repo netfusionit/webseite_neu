3916c8b2f75b164afb7dfa9870f8e6c952faaa02
Florian Scheibel <florian.scheibel@outlook.de>
2024-06-01 21:37:10 +0200

    0

--- blog-details.php
+++ blog-details.php
@@ -184,6 +184,7 @@
         }
 
         .blog-section {
+            background-color: #e9ecef;
             padding: 40px 0;
         }
 
@@ -254,6 +255,23 @@
                 echo "</div>";
                 echo "<div class='blog-content'>" . $row['content'] . "</div>";
 
+                // Reaktionen anzeigen
+                echo "<div class='reaction' id='reactions'>";
+                $reactions = ['like' => 'bi-hand-thumbs-up', 'love' => 'bi-heart', 'wow' => 'bi-emoji-sunglasses', 'sad' => 'bi-emoji-frown'];
+                foreach ($reactions as $reaction => $icon) {
+                    echo "<span class='bi $icon' data-reaction='$reaction' data-blog-id='$id'><span class='count'></span></span>";
+                }
+                echo "</div>";
+
+                // Tags anzeigen
+                echo "<div class='blog-tags'>";
+                echo "<div class='tags'>";
+                $tags = explode(',', $row['tags']);
+                foreach ($tags as $tag) {
+                    echo "<span>$tag</span>";
+                }
+                echo "</div>";
+                echo "</div>";
 
                 // Footer mit Trennlinie und Reaktionen
                 echo "<div class='blog-footer bg-dark-footer'>";
@@ -272,7 +290,6 @@
                 echo "</div>";
 
                 // Kommentarformular
-                echo "<h3 class='mt-5'>Kommentare</h3>";
                 echo "<div class='toggle-comment'>";
                 echo "<h3>Neuer Kommentar</h3><i class='fas fa-chevron-up toggle-icon'></i>";
                 echo "</div>";
@@ -296,6 +313,7 @@
                 echo "</div>";
 
                 // Kommentare anzeigen
+                echo "<h3 class='mt-5'>Kommentare</h3>";
                 $comment_result = $conn->query("SELECT * FROM comments WHERE blog_id = $id ORDER BY created_at DESC");
                 while ($comment = $comment_result->fetch_assoc()) {
                     echo "<div class='comment-box'>";
