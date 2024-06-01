1dcbd21a409e941a1269fb1a86e1d4211b893b9f
Florian Scheibel <florian.scheibel@outlook.de>
2024-06-01 21:43:24 +0200

    Lokig geändert

--- add_reaction.php
+++ add_reaction.php
@@ -3,25 +3,25 @@ include 'db.php';
 
 $blog_id = isset($_POST['blog_id']) ? intval($_POST['blog_id']) : null;
 $reaction_type = $_POST['reaction_type'];
-$comment_id = isset($_POST['comment_id']) ? intval($_POST['comment_id']) : null;
 
-if ($comment_id) {
-    // Reaktionen für Kommentare
-    $sql = "INSERT INTO comment_reactions (comment_id, reaction_type) VALUES ('$comment_id', '$reaction_type')";
-    if ($conn->query($sql) === TRUE) {
-        echo "Reaktion gespeichert";
+$response = ['success' => false];
+
+if ($blog_id) {
+    // Prüfen, ob bereits ein Datensatz für diesen Artikel vorhanden ist
+    $result = $conn->query("SELECT * FROM article_reactions WHERE blog_id = $blog_id");
+    if ($result->num_rows > 0) {
+        // Reaktion hochzählen
+        $sql = "UPDATE article_reactions SET {$reaction_type}_count = {$reaction_type}_count + 1 WHERE blog_id = $blog_id";
     } else {
-        echo "Fehler: " . $sql . "<br>" . $conn->error;
+        // Neuen Datensatz erstellen
+        $sql = "INSERT INTO article_reactions (blog_id, {$reaction_type}_count) VALUES ($blog_id, 1)";
     }
-} else if ($blog_id) {
-    // Reaktionen für Blog-Posts
-    $sql = "INSERT INTO reactions (blog_id, reaction_type) VALUES ('$blog_id', '$reaction_type')";
+
     if ($conn->query($sql) === TRUE) {
-        echo "Reaktion gespeichert";
-    } else {
-        echo "Fehler: " . $sql . "<br>" . $conn->error;
+        $response['success'] = true;
     }
 }
 
+echo json_encode($response);
 $conn->close();
 ?>
--- load_reactions.php
+++ load_reactions.php
@@ -3,13 +3,8 @@ include 'db.php';
 
 $blog_id = intval($_GET['blog_id']);
 
-$reactions = ['like', 'love', 'wow', 'sad'];
-$data = [];
-
-foreach ($reactions as $reaction) {
-    $result = $conn->query("SELECT COUNT(*) as count FROM reactions WHERE blog_id = $blog_id AND reaction_type = '$reaction'");
-    $data[$reaction] = $result->fetch_assoc()['count'];
-}
+$result = $conn->query("SELECT like_count, love_count, wow_count, sad_count FROM article_reactions WHERE blog_id = $blog_id");
+$data = $result->fetch_assoc();
 
 echo json_encode($data);
 ?>
