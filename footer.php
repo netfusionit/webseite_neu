c21122f3c0e01eff9ec1b9fc41fa2da8ef5d8a05
Florian Scheibel <florian.scheibel@outlook.de>
2024-06-02 14:17:24 +0200

    Footer Update

--- footer.php
+++ footer.php
@@ -1,36 +1,41 @@
-<footer class="bg-light text-center text-lg-start">
+<footer class="bg-light text-lg-start">
     <div class="container p-4">
         <div class="row">
             <!-- Company Information -->
             <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
-                <h5 class="text-uppercase">NetFusionIT</h5>
+                <h5 class="text-uppercase font-weight-bold">NetFusionIT</h5>
                 <p>
-                    NetFusionIT Internet Dienstleistungen GmbH <br>
-                    Kaiserstraße 57/1 <br>
-                    72764 Reutlingen <br>
+                    NetFusionIT Internet Dienstleistungen GmbH<br>
+                    Kaiserstraße 57/1<br>
+                    72764 Reutlingen<br>
                     Deutschland
                 </p>
                 <p>
-                    Handelsregister: AG Stuttgart, HRB 361183 <br>
+                    Handelsregister: AG Stuttgart, HRB 361183<br>
                     USt-IdNr.: DE203032334
                 </p>
             </div>
 
             <!-- Contact Information -->
             <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
-                <h5 class="text-uppercase">So erreichen Sie uns:</h5>
+                <h5 class="text-uppercase font-weight-bold">So erreichen Sie uns:</h5>
                 <p>Telefon: <a href="tel:+497121261970">+49 7121 26197-0</a></p>
                 <p>E-Mail: <a href="mailto:vertrieb@cmo.de">vertrieb@cmo.de</a></p>
                 <p>Buchhaltung: <a href="#">Ticket eröffnen</a></p>
                 <p>Technik: <a href="#">Ticket eröffnen</a></p>
-                <h5 class="text-uppercase mt-3">Unsere Bürozeiten:</h5>
+                <h5 class="text-uppercase font-weight-bold mt-3">Unsere Bürozeiten:</h5>
                 <p>Mo.-Do.: 09:00 Uhr bis 17:00 Uhr</p>
                 <p>Freitag: 09:00 Uhr bis 15:00 Uhr</p>
             </div>
 
-            <!-- Social Media and Additional Links -->
+            <!-- Search and Useful Links -->
             <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
-                <h5 class="text-uppercase mt-3">Nützliche Links:</h5>
+                <h5 class="text-uppercase font-weight-bold">Suchen Sie auf unserer Seite:</h5>
+                <form class="form-inline">
+                    <input class="form-control mr-sm-2 w-75" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+                </form>
+                <h5 class="text-uppercase font-weight-bold mt-3">Nützliche Links:</h5>
                 <ul class="list-unstyled">
                     <li><a href="#!">Home</a></li>
                     <li><a href="#!">Über uns</a></li>
@@ -40,10 +45,10 @@
                 </ul>
             </div>
         </div>
-        <hr class="my-4" />
+        <hr class="my-4">
         <div class="row">
-            <div class="col-md-12">
-                <p class="text-center">&copy; 2024 NetFusionIT. Alle Rechte vorbehalten.</p>
+            <div class="col-md-12 text-center">
+                <p>&copy; 2024 NetFusionIT. Alle Rechte vorbehalten.</p>
             </div>
         </div>
     </div>
