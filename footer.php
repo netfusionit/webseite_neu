bdea881455508ec030ba57085c4b89bee1ec3b65
Florian Scheibel <florian.scheibel@outlook.de>
2024-06-02 14:08:17 +0200

    0

--- footer.php
+++ footer.php
@@ -1,7 +1,56 @@
 <footer class="bg-light text-center text-lg-start">
     <div class="container p-4">
-        <p>&copy; 2024 NetFusionIT. Alle Rechte vorbehalten.</p>
-    </div>
+        <div class="row">
+            <!-- Company Information -->
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">NetFusionIT</h5>
+                <p>
+                    NetFusionIT Internet Dienstleistungen GmbH <br>
+                    Kaiserstraße 57/1 <br>
+                    72764 Reutlingen <br>
+                    Deutschland
+                </p>
+                <p>
+                    Handelsregister: AG Stuttgart, HRB 361183 <br>
+                    USt-IdNr.: DE203032334
+                </p>
+            </div>
 
-    
+            <!-- Contact Information -->
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">So erreichen Sie uns:</h5>
+                <p>Telefon: <a href="tel:+497121261970">+49 7121 26197-0</a></p>
+                <p>E-Mail: <a href="mailto:vertrieb@cmo.de">vertrieb@cmo.de</a></p>
+                <p>Buchhaltung: <a href="#">Ticket eröffnen</a></p>
+                <p>Technik: <a href="#">Ticket eröffnen</a></p>
+                <h5 class="text-uppercase mt-3">Unsere Bürozeiten:</h5>
+                <p>Mo.-Do.: 09:00 Uhr bis 17:00 Uhr</p>
+                <p>Freitag: 09:00 Uhr bis 15:00 Uhr</p>
+            </div>
+
+            <!-- Social Media and Additional Links -->
+            <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Folgen Sie uns:</h5>
+                <a href="#" class="me-3"><i class="fab fa-facebook-f"></i></a>
+                <a href="#" class="me-3"><i class="fab fa-twitter"></i></a>
+                <a href="#" class="me-3"><i class="fab fa-youtube"></i></a>
+                <a href="#" class="me-3"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-tiktok"></i></a>
+                <h5 class="text-uppercase mt-3">Nützliche Links:</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#!">Home</a></li>
+                    <li><a href="#!">Über uns</a></li>
+                    <li><a href="#!">Services</a></li>
+                    <li><a href="#!">Datenschutzerklärung</a></li>
+                    <li><a href="#!">Impressum</a></li>
+                </ul>
+            </div>
+        </div>
+        <hr class="my-4" />
+        <div class="row">
+            <div class="col-md-12">
+                <p class="text-center">&copy; 2024 NetFusionIT. Alle Rechte vorbehalten.</p>
+            </div>
+        </div>
+    </div>
 </footer>
