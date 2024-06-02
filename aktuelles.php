d823b0eb812dea8523e1757748d791327a201214
Florian Scheibel <florian.scheibel@outlook.de>
2024-06-02 15:44:36 +0200

    0

--- aktuelles.php
+++ aktuelles.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Alle Aktuellen Meldungen im Überblick - NetFusionIT</title>
