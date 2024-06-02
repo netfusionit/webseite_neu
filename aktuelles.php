1be84938556574426434d30571ef7b8cf0889d51
Florian Scheibel <florian.scheibel@outlook.de>
2024-06-02 15:19:51 +0200

    03

--- aktuelles.php
+++ aktuelles.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alle Aktuellen Meldungen im Überblick - NetFusionIT</title>
+    <title>Aktuelles - NetFusionIT</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="styles.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+</head>
     <style>
         body {
             font-family: 'Arial', sans-serif;
