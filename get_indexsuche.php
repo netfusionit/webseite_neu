90cb8b226f64422fd736114a883ca11d55a597b8
Florian Scheibel <florian.scheibel@outlook.de>
2024-06-02 21:19:34 +0200

    update indexsuche

--- get_indexsuche.php
+++ get_indexsuche.php
@@ -13,8 +13,23 @@ if ($indexPath !== false && file_exists($indexPath)) {
         $lines = explode("\n", $indexContent);
         foreach ($lines as $lineNumber => $line) {
             if (stripos($line, $search) !== false) {
+                // Extract 3-4 sentences around the search term
+                $cleanLine = strip_tags($line);
+                $sentences = preg_split('/([.!?])/', $cleanLine, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
+                $resultSnippet = '';
+                $found = false;
+
+                foreach ($sentences as $i => $sentence) {
+                    if (stripos($sentence, $search) !== false) {
+                        $resultSnippet .= (isset($sentences[$i-1]) ? $sentences[$i-1] : '') . (isset($sentences[$i-2]) ? $sentences[$i-2] : '') . $sentence . (isset($sentences[$i+1]) ? $sentences[$i+1] : '') . (isset($sentences[$i+2]) ? $sentences[$i+2] : '');
+                        $found = true;
+                        break;
+                    }
+                }
+
+                $highlightedSnippet = str_ireplace($query, "<mark>$query</mark>", $resultSnippet);
                 $results[] = [
-                    'line' => $line,
+                    'line' => $highlightedSnippet,
                     'line_number' => $lineNumber + 1
                 ];
             }
