245cd1cbd7f0b2aa163898eb8bfcdef34cf3468a
Florian Scheibel <florian.scheibel@outlook.de>
2024-06-02 15:47:35 +0200

    0

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
                 <a class="nav-link" href="aktuelles.php">Aktuelles</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="administration.php">Administration</a>
+                <a class="nav-link" href="admin.php">Administration</a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="servicesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
