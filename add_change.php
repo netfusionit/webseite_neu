d16af978f0e4bb15016c561a49f6ce13846b1201
Florian Scheibel <florian.scheibel@outlook.de>
2024-06-02 17:05:32 +0200

    Verfollständigung der add_chnaglog-Funktion

--- add_change.php
+++ add_change.php
@@ -6,3 +6,13 @@ $active_since = $_POST['active_since'];
 $category = $_POST['category'];
 $description = $_POST['description'];
 
+$sql = "INSERT INTO changelog (title, active_since, category, description) VALUES ('$title', '$active_since', '$category', '$description')";
+if ($conn->query($sql) === TRUE) {
+    echo "Neue Änderung erfolgreich hinzugefügt.";
+} else {
+    echo "Fehler: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+header("Location: admin.php");
+?>
