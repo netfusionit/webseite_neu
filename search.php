6987ce767a886190747fe5ec0e141c6d9d31b47b
Florian Scheibel <florian.scheibel@outlook.de>
2024-06-02 21:03:55 +0200

    änderungSearch

--- search.php
+++ search.php
@@ -114,23 +114,15 @@
             $stmt->close();
 
             // Suche in index.php für allgemeine Seiteninhalte
-            $indexContent = file_get_contents('https://netfusionit.de/index.php');
+            $indexPath = realpath(dirname(__FILE__) . '/../index.php');
+            if ($indexPath !== false && file_exists($indexPath)) {
+                $indexContent = file_get_contents($indexPath);
 
-            if ($indexContent !== false) {
-                $dom = new DOMDocument();
-                libxml_use_internal_errors(true);
-                $dom->loadHTML($indexContent);
-                libxml_clear_errors();
-
-                $xpath = new DOMXPath($dom);
-                $body = $dom->getElementsByTagName('body')->item(0);
-                $texts = $xpath->query('//text()[contains(., "' . $query . '")]');
-
-                if ($texts->length > 0) {
+                if ($indexContent !== false && stripos($indexContent, $query) !== false) {
                     echo "<div class='result-item'>";
                     echo "<h3>Startseite</h3>";
                     echo "<p>Der Suchbegriff wurde auf der Startseite gefunden.</p>";
-                    echo "<br><a href='https://netfusionit.de/index.php' class='btn btn-primary mt-2'>Zum Seiteninhalt springen</a>";
+                    echo "<br><a href='/index.php' class='btn btn-primary mt-2'>Zum Seiteninhalt springen</a>";
                     echo "</div>";
                 } else {
                     echo "<p>Keine relevanten Inhalte auf der Startseite gefunden.</p>";
