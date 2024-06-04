bababd1aa0417bf7569f29499d5bf6f02181dc5c
Florian Scheibel <florian.scheibel@outlook.de>
2024-06-04 15:37:15 +0200

    Inhalt Teil 2

--- index.php
+++ index.php
@@ -120,20 +120,19 @@
 
     <!-- About Section -->
     <section id="about" class="about section">
-        <div class="container">
-            <div class="row gy-4">
-                <div class="col-lg-6 content" data-aos="fade-up" data-aos-delay="100">
-                    <p class="who-we-are">Who We Are</p>
-                    <h3>Unleashing Potential with Creative Strategy</h3>
-                    <p class="fst-italic">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua.
-                    </p>
-                    <ul>
-                        <li><i class="bi bi-check-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></li>
-                        <li><i class="bi bi-check-circle"></i> <span>Duis aute irure dolor in reprehenderit in voluptate velit.</span></li>
-                        <li><i class="bi bi-check-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</span></li>
-                    </ul>
+    <div class="container">
+        <div class="row gy-4">
+            <div class="col-lg-6 content" data-aos="fade-up" data-aos-delay="100">
+                <p class="who-we-are">ÜBER NetFusionIT</p>
+                <h3>Potential freisetzen mit kreativer Strategie</h3>
+                <p class="fst-italic">
+                    Wir bieten maßgeschneiderte Lösungen im Bereich Netzwerktechnik und Internetdienstleistungen, um Ihr Unternehmen auf die nächste Stufe zu heben.
+                </p>
+                <ul>
+                    <li><i class="bi bi-check-circle"></i> <span>Professionelle Netzwerktechnik und Internetdienstleistungen.</span></li>
+                    <li><i class="bi bi-check-circle"></i> <span>Modernes Wordpress-Webdesign für einen starken Online-Auftritt.</span></li>
+                    <li><i class="bi bi-check-circle"></i> <span>Umfassender Service, Support und Wartung für dauerhafte Zuverlässigkeit.</span></li>
+                </ul>
                     <a href="#" class="read-more"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
                 </div>
                 <div class="col-lg-6 about-images" data-aos="fade-up" data-aos-delay="200">
