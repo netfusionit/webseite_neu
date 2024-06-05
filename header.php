942cf57c55b92e699884b513b4d8ee01c8702318
Florian Scheibel <florian.scheibel@outlook.de>
2024-06-05 21:20:05 +0200

    update

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 <a class="navbar-brand" href="index.php">
-        <img src="/img/LogoEntwurf_NetFusionIT_small.png" alt="NetFusionIT Logo" style="height: 40px;">
+        <img src="/img/logo_transparent_neu.png" alt="NetFusionIT Logo" style="height: 40px;">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
